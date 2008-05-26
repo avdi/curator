cc9a306ab0ecea6b41c15bfbf72d27df7aac5398
Avdi Grimm <avdi@avdi.org>
2008-05-25 23:26:49 -0400

    blah

--- application.rb
+++ application.rb
@@ -1,21 +1,61 @@
 require 'rubygems'
 require 'sinatra'
+require 'builder'
 require 'markaby'
 
-module MarkabyRenderer
-  def mab(options = {}, &template)
-    render(:mab, template, options)
-  end
-
-  def render_mab(template)
-    Markaby::Template.new(template).render({}, self).to_s
+# Monkeypatch Markaby so as not to collide with XML::Builder renderer
+class Markaby::Builder
+  def capture(&block)
+    @streams.push(@builder.target = [])
+    @builder.level += 1
+    str = instance_eval(&block)
+    str = @streams.last.join if @streams.last.any?
+    @streams.pop
+    @builder.level -= 1
+    @builder.target = @streams.last
+    str
   end
 end
-Sinatra::EventContext.send :include, MarkabyRenderer
 
+module MarkabyRenderer
+  def mab(content=nil, options={}, &block)
+    content = Proc.new { block } if content.nil?
+    render(:mab, content, options)
+  end
+
+  def render_mab(content, options ={}, &block)
+    output = Markaby::Builder.new(options.fetch(:locals){{}},
+                                  options.fetch(:scope){self})
+    output.tagset = Markaby::XHTMLStrict
+    case content
+    when String then output.instance_eval(content)
+    when Proc then output.capture(&content)
+    else raise NotImplementedError
+    end
+  end
+end
+class Sinatra::EventContext
+  include MarkabyRenderer
+end
+
+helpers do
+  def name
+    "Curator"
+  end
+end
+
+template :layout do
+<<-END
+    div do
+      div "Above"
+      self << yield
+      div "Below"
+    end
+END
+end
 
 get '/' do
   mab do
-    h1 "Welcome to Curator!"
+    div.foo! "Welcome to  #{name}"
   end
 end
